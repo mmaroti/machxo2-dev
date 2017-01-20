15a65edac7b4aaea13272d42d844743fda2755f0
Miklos Maroti <mmaroti@gmail.com>
2017-01-19 22:05:57 -0600

    ok, created a project (manually edited for blink app)

---
+++ blink.v
@@ -0,0 +1,16 @@
+module top(output reg [7:0] leds);
+
+wire clk;
+OSCH #(.NOM_FREQ("133.00")) rc_osc(.STDBY(1'b0), .OSC(clk), .SEDSTDBY());
+
+reg [29:0] counter;
+
+initial counter <= 0;
+
+always @(posedge clk)
+begin
+	counter <= counter + 1;
+	leds <= counter[29:22];
+end
+
+endmodule
