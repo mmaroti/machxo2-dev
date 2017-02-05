68b19eeda6ed406e640dc7d1ed51f9296eca69e6
Miklos Maroti <mmaroti@gmail.com>
2017-02-04 22:20:48 -0600

    playing around with pipes

--- verilog/channel.v
+++ verilog/channel.v
@@ -72,3 +72,73 @@ module pull2chan #(parameter WIDTH = 8) (
 	end
 
 endmodule
+
+module buffer #(parameter integer WIDTH = 8, SIZE = 3, SIZE_WIDTH = $clog2(SIZE + 1)) (
+	input wire clock,
+	input wire resetn,
+	output reg [SIZE_WIDTH-1:0] size,
+	input wire [WIDTH-1:0] idata,
+	input wire ivalid,
+	output reg iready,
+	output reg [WIDTH-1:0] odata,
+	output reg ovalid,
+	input wire oready);
+
+	integer i;
+
+	wire itransfer = ivalid && iready;
+	wire otransfer = ovalid && oready;
+	
+	wire [SIZE_WIDTH-1:0] size2 = size - otransfer;
+	wire [SIZE_WIDTH-1:0] size3 = size2 + itransfer;
+
+	always @(posedge clock or negedge resetn)
+	begin
+		if (!resetn)
+		begin
+			size <= 1'b0;
+			iready <= 1'b0;
+			ovalid <= 1'b0;
+		end
+		else
+		begin
+			size <= size3;
+			iready <= size3 != SIZE;
+			ovalid <= size3 != 0;
+		end
+	end
+
+	reg [WIDTH-1:0] buffer[1:SIZE-1];
+	always @(posedge clock or negedge resetn)
+	begin
+		if (!resetn)
+		begin
+			for (i = 1; i < SIZE; i = i + 1)
+				buffer[i] <= {WIDTH{1'bx}};
+		end
+		else if (itransfer)
+		begin
+			buffer[1] <= idata;
+			for (i = 2; i < SIZE; i = i + 1)
+				buffer[i] <= buffer[i - 1];
+		end
+	end
+
+	reg [WIDTH-1:0] buffer2[0:SIZE];
+	always @(*)
+	begin
+		buffer2[0] = idata;
+		for (i = 1; i < SIZE; i = i + 1)
+			buffer2[i] = buffer[i];
+		buffer2[SIZE] = odata;
+	end
+
+	always @(posedge clock or negedge resetn)
+	begin
+		if (!resetn)
+			odata <= {WIDTH{1'bx}};
+		else if (otransfer)
+			odata <= buffer2[size2];
+	end
+
+endmodule
