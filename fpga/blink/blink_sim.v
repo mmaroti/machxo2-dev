465fed1019adb97a793935d07dbb4716da2124d9
Miklos Maroti <mmaroti@gmail.com>
2017-02-13 22:31:32 -0600

    added simulation screendump

--- fpga/blink/blink_sim.v
+++ fpga/blink/blink_sim.v
@@ -1,6 +1,13 @@
+`timescale 1ns/1ps
 module sim();
 
 wire [7:0] leds;
 top top(.leds(leds));
+
+initial
+begin
+	#100	// wait 100 nanoseconds, one clock tick is about 1/133000000 = 7518 ps
+	$finish;
+end
 
 endmodule
